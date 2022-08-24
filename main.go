a5fd28875cbfcf9b69344c395e9dea46009e637f
IshlahulHanif <ishlahul.hanif@tokopedia.com>
2022-08-24 23:02:51 +0700

    add version

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 )
 
 func main() {
+	version := "1.0.11"
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello risk-tech\n")
 	})
 
-	fmt.Println("RSMN demo service running")
+	fmt.Printf("RSMN demo version %v service running\n", version)
 
 	log.Fatal(http.ListenAndServe(":5555", nil))
 
